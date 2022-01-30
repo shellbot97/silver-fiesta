229dbc845136dd84f38b7774b5a5446bf4be0e00
MEDIA\hitesh.i <hitesh.i@media.net>
2022-01-30 23:20:43 +0530

    new course

--- width-height.js
+++
@@ -1,6 +0,0 @@
-console.log('height', window.innerHeight);
-console.log('width', window.innerWidth);
-document.querySelector('.btn').addEventListener('click', () => {
-    const dimensions = document.querySelector('.box').getBoundingClientRect()
-    console.log(dimensions);
-})
